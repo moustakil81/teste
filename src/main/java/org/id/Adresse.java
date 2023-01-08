a6fd65d598575046dc158a9a42c7b131c93741ac
moustakil81 <moustakil.domont@gmail.com>
2023-01-08 17:04:50 +0100

    getters

--- src/main/java/org/id/Adresse.java
+++ src/main/java/org/id/Adresse.java
@@ -6,6 +6,39 @@ public class Adresse {
 	private String name;
 	private String rue;
 	private String dep;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public String getDep() {
+		return dep;
+	}
+
+	public void setDep(String dep) {
+		this.dep = dep;
+	}
+
 	public Adresse() {
 		// TODO Auto-generated constructor stub
 	}
